c82dbb5634f521071d7ea630541a80c2ce749d2a
Luke Moule <65358800+LukeMoule@users.noreply.github.com>
2023-08-04 09:37:34 +0100

    Added leaflet map

--- logic.js
+++ logic.js
@@ -74,3 +74,9 @@ async function main() {
 
 
 main();
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap'
+}).addTo(map);
